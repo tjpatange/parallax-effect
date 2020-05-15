3a98c2a190fe51879abc92ed5db2b6cc9584e940
tjpatange <tejashripatange@gmail.com>
2020-05-15 12:24:42 +0200

    3 files modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,6 @@ export class AppComponent  {
    div1: ParallaxConfig = {ratio: 0.1, init: 0, css: 'transform:translateY'};
   div2: ParallaxConfig = {ratio: 0.5, init: -100, css: 'background-position-y'};
   p: ParallaxConfig = {ratio: 0.9, init: 100, css: 'top'};
+
+  url='https://en.wikipedia.org/wiki/List_of_highest_mountains_on_Earth#/media/File:Mount_Everest_as_seen_from_Drukair2_PLW_edit.jpg';
 }
